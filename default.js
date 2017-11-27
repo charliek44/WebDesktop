da30133ad700851fa025fa6009e86eb239fc0179
charliek44 <33808271+charliek44@users.noreply.github.com>
2017-11-28 00:21:59 +0800

    Updated default.js
    Changed to https

--- default.js
+++ default.js
@@ -4,7 +4,7 @@ $section = $(".section");
 
 $section.on("click", function() {
     $.ajax({
-        url: "http://ron-swanson-quotes.herokuapp.com/v2/quotes",
+        url: "https://ron-swanson-quotes.herokuapp.com/v2/quotes",
         success: function(quoteData) {
             var post = quoteData[0];
             $('#quote-quote').text(post);
