1c273371047898a857b1b71f5ba4e9a7718ad181
charliek44 <33808271+charliek44@users.noreply.github.com>
2017-11-28 00:06:54 +0800

    Update default.js

--- default.js
+++ default.js
@@ -4,18 +4,14 @@ $section = $(".section");
 
 $section.on("click", function() {
     $.ajax({
-        url: "https://market.mashape.com/andruxnet/random-famous-quotes",
+        url: "https://api.icndb.com",
         success: function(quoteData) {
             var post = quoteData.shift();
-            $('#quote-quote').text(post.quote);
-            $('#quote-author').html(post.author);
+            $('#quote-quote').text(post.joke);
+            $('#quote-author').html("Chuck Norris");
         },
         error: function() {
             alert("error");
-        },
-        headers: {
-            'X-Mashape-Key': 'XYveoSjCABmshwrDJFgmIsbWZKMdp1rSeVIjsnrf8gABgYLbVM',
-            'Accept': 'application/json'
         },
         cache: false,
         type: 'GET'
