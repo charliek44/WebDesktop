b02cc6ec09d30016dcf870915449196a486f2cf6
charliek44 <33808271+charliek44@users.noreply.github.com>
2017-11-28 09:04:49 +0800

    Random Quotes

--- default.js
+++ default.js
@@ -1,6 +1,8 @@
 $(document).ready();
 
 $section = $(".section");
+$tweet = $("#tweet");
+$tweetLink = $(".twitter-share-button");
 
 $section.on("click", getQuote);
 
@@ -9,7 +11,10 @@ function getQuote() {
         url: "https://ron-swanson-quotes.herokuapp.com/v2/quotes",
         success: function(quoteData) {
             var post = quoteData[0];
+            var tweeturl = "https://twitter.com/intent/tweet?text=";
             $('#quote-quote').text(post);
+            tweeturl += "\"" + post + "\" - Ron Swanson";
+            $tweetLink.attr("href", tweeturl)
         },
         error: function() {
             alert("error");
@@ -20,3 +25,20 @@ function getQuote() {
 };
 
 $(document).ready(getQuote);
+
+function imgBold() {
+    $tweet.css({
+        "width": "30px",
+        "height": "30px"
+    });
+};
+
+function imgNormal() {
+    $tweet.css({
+        "width": "25px",
+        "height": "25px"
+    });
+};
+
+$tweet.on("mouseover", imgBold);
+$tweet.on("mouseout", imgNormal);
